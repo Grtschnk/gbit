a02ef450d7ea2299e2dc866ce93db7745f38dbba
David <david.tomzik@gmail.com>
2023-04-01 07:49:56 +1300

    Auto format

--- lib/tester.c
+++ lib/tester.c
@@ -1,22 +1,22 @@
 
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <assert.h>
 
 #include "common.h"
-#include "tester.h"
 #include "disassembler.h"
-#include "ref_cpu.h"
 #include "inputstate.h"
 #include "instructions.h"
+#include "ref_cpu.h"
+#include "tester.h"
 
 #define INSTRUCTION_MEM_SIZE 4
 static u8 instruction_mem[INSTRUCTION_MEM_SIZE];
 
 static unsigned long num_tests = 0;
-static bool tested_op[256] = { 0 };
-static bool tested_op_cb[256] = { 0 };
+static bool tested_op[256] = {0};
+static bool tested_op_cb[256] = {0};
 
 static struct tester_operations *tcpu_ops;
 static struct tester_flags *flags;
@@ -24,25 +24,36 @@ static struct tester_flags *flags;
 static void dump_state(struct state *state)
 {
     printf(" PC   SP   AF   BC   DE   HL  ZNHC hlt IME\n"
-            "%04x %04x %04x %04x %04x %04x %d%d%d%d  %d   %d\n",
-            state->PC, state->SP, state->reg16.AF, state->reg16.BC,
-            state->reg16.DE, state->reg16.HL,
-            BIT(state->reg16.AF, 7), BIT(state->reg16.AF, 6),
-            BIT(state->reg16.AF, 5), BIT(state->reg16.AF, 4), state->halted,
-            state->interrupts_master_enabled);
+           "%04x %04x %04x %04x %04x %04x %d%d%d%d  %d   %d\n",
+           state->PC,
+           state->SP,
+           state->reg16.AF,
+           state->reg16.BC,
+           state->reg16.DE,
+           state->reg16.HL,
+           BIT(state->reg16.AF, 7),
+           BIT(state->reg16.AF, 6),
+           BIT(state->reg16.AF, 5),
+           BIT(state->reg16.AF, 4),
+           state->halted,
+           state->interrupts_master_enabled);
 
-    for (int i = 0; i < state->num_mem_accesses; i++)
-        printf("  Mem %s: addr=%04x val=%02x\n",
-                state->mem_accesses[i].type ? "write" : "read",
-                state->mem_accesses[i].addr, state->mem_accesses[i].val);
+    if (true)
+    {
+        for (int i = 0; i < state->num_mem_accesses; i++)
+            printf("  Mem %s: addr=%04x val=%02x\n",
+                   state->mem_accesses[i].type ? "write" : "read",
+                   state->mem_accesses[i].addr,
+                   state->mem_accesses[i].val);
+    }
     printf("\n");
 }
 
 static void dump_op_state(struct test_inst *inst, struct op_state *op_state)
 {
-    const char *reg8_names[] = { "B", "C", "D", "E", "H", "L", "(HL)", "A" };
-    const char *reg16_names[] = { "BC", "DE", "HL", "SP/AF" };
-    const char *cond_names[] = { "NZ", "Z", "NC", "C" };
+    const char *reg8_names[] = {"B", "C", "D", "E", "H", "L", "(HL)", "A"};
+    const char *reg16_names[] = {"BC", "DE", "HL", "SP/AF"};
+    const char *cond_names[] = {"NZ", "Z", "NC", "C"};
 
     if (inst->imm_size == 1)
         printf("imm: %02x\n", op_state->imm);
@@ -67,10 +78,12 @@ static void dump_op_state(struct test_inst *inst, struct op_state *op_state)
 
 static int mem_access_cmp(const void *p1, const void *p2)
 {
-    u16 addr1 = ((struct mem_access*)p1)->addr;
-    u16 addr2 = ((struct mem_access*)p2)->addr;
-    if (addr1 < addr2) return -1;
-    if (addr1 > addr2) return 1;
+    u16 addr1 = ((struct mem_access *)p1)->addr;
+    u16 addr2 = ((struct mem_access *)p2)->addr;
+    if (addr1 < addr2)
+        return -1;
+    if (addr1 > addr2)
+        return 1;
     return 0;
 }
 
@@ -84,10 +97,8 @@ static int states_mem_accesses_eq(struct state *s1, struct state *s2)
     /* Sort memory accesses by address before comparing them. Usually the order
      * of memory accesses do not matter (e.g., the order of bytes of a 16-bit
      * store), so this reduces false positives. */
-    qsort(s1->mem_accesses, s1->num_mem_accesses, sizeof(struct mem_access),
-            mem_access_cmp);
-    qsort(s2->mem_accesses, s2->num_mem_accesses, sizeof(struct mem_access),
-            mem_access_cmp);
+    qsort(s1->mem_accesses, s1->num_mem_accesses, sizeof(struct mem_access), mem_access_cmp);
+    qsort(s2->mem_accesses, s2->num_mem_accesses, sizeof(struct mem_access), mem_access_cmp);
 
     for (i = 0; i < s1->num_mem_accesses; i++)
         if (s1->mem_accesses[0].type != s2->mem_accesses[0].type ||
@@ -100,13 +111,9 @@ static int states_mem_accesses_eq(struct state *s1, struct state *s2)
 
 static int states_eq(struct state *s1, struct state *s2)
 {
-    return s1->reg16.AF == s2->reg16.AF &&
-           s1->reg16.BC == s2->reg16.BC &&
-           s1->reg16.DE == s2->reg16.DE &&
-           s1->reg16.HL == s2->reg16.HL &&
-           s1->PC == s2->PC &&
-           s1->SP == s2->SP &&
-           s1->halted == s2->halted &&
+    return s1->reg16.AF == s2->reg16.AF && s1->reg16.BC == s2->reg16.BC &&
+           s1->reg16.DE == s2->reg16.DE && s1->reg16.HL == s2->reg16.HL && s1->PC == s2->PC &&
+           s1->SP == s2->SP && s1->halted == s2->halted &&
            s1->interrupts_master_enabled == s2->interrupts_master_enabled &&
            states_mem_accesses_eq(s1, s2);
 }
@@ -121,7 +128,6 @@ static void op_state_reset(struct op_state *op_state)
     memset(op_state, 0, sizeof(*op_state));
 }
 
-
 static int run_state(struct state *state)
 {
     struct state tcpu_out_state, rcpu_out_state;
@@ -135,7 +141,8 @@ static int run_state(struct state *state)
     tcpu_ops->get_state(&tcpu_out_state);
     rcpu_get_state(&rcpu_out_state);
 
-    if (!states_eq(&tcpu_out_state, &rcpu_out_state)) {
+    if (!states_eq(&tcpu_out_state, &rcpu_out_state))
+    {
         printf("\n  === STATE MISMATCH ===\n");
         printf("\n - Instruction -\n");
         disassemble(instruction_mem);
@@ -191,7 +198,8 @@ static int test_instruction(struct test_inst *inst)
 
     state_reset(&state);
     op_state_reset(&op_state);
-    do {
+    do
+    {
         u8 opcode;
 
         assemble(instruction_mem, inst, &op_state);
@@ -205,7 +213,8 @@ static int test_instruction(struct test_inst *inst)
 
         num_tests++;
 
-        if (flags->print_verbose_inputs) {
+        if (flags->print_verbose_inputs)
+        {
             dump_op_state(inst, &op_state);
             disassemble(instruction_mem);
         }
@@ -214,8 +223,9 @@ static int test_instruction(struct test_inst *inst)
         u8 imem_old[INSTRUCTION_MEM_SIZE] = {0};
         memcpy(imem_old, instruction_mem, INSTRUCTION_MEM_SIZE);
         last_op_had_failure = run_state(&state);
-        //if instruction mem was modified
-        if (memcmp(imem_old, instruction_mem, INSTRUCTION_MEM_SIZE)) {
+        // if instruction mem was modified
+        if (memcmp(imem_old, instruction_mem, INSTRUCTION_MEM_SIZE))
+        {
             printf("Ouch, your testcpu should NOT write to instruction_mem\n");
             exit(1);
         }
@@ -223,7 +233,8 @@ static int test_instruction(struct test_inst *inst)
         last_op = opcode;
         op_success_table[opcode] = !last_op_had_failure;
 
-        if (last_op_had_failure) {
+        if (last_op_had_failure)
+        {
             had_failure = true;
             if (!flags->keep_going_on_mismatch)
                 return 1;
@@ -233,24 +244,25 @@ static int test_instruction(struct test_inst *inst)
     return had_failure;
 }
 
-static int test_instructions(size_t num_instructions,
-                             struct test_inst *insts,
-                             const char *prefix)
+static int test_instructions(size_t num_instructions, struct test_inst *insts, const char *prefix)
 {
     size_t num_instructions_tested = 0;
     size_t num_instructions_passed = 0;
 
-    for (size_t i = 0; i < num_instructions; i++) {
+    for (size_t i = 0; i < num_instructions; i++)
+    {
         bool failure;
 
-        if (flags->print_tested_instruction) {
+        if (flags->print_tested_instruction)
+        {
             printf("%s   ", insts[i].mnem);
             if (insts[i].is_cb_prefix)
                 printf("(CB prefix)");
             printf("\n");
         }
 
-        if (!insts[i].enabled) {
+        if (!insts[i].enabled)
+        {
             if (flags->print_tested_instruction)
                 printf(" Skipping\n");
             continue;
@@ -272,11 +284,11 @@ static int test_instructions(size_t num_instructions,
     if (flags->print_tested_instruction)
         printf("\n");
 
-    printf("Tested %zu/%zu %sinstructions", num_instructions_tested,
-            num_instructions, prefix);
+    printf("Tested %zu/%zu %sinstructions", num_instructions_tested, num_instructions, prefix);
     if (num_instructions_passed != num_instructions_tested)
-        printf(", %zu passed and %zu failed", num_instructions_passed,
-                num_instructions_tested - num_instructions_passed);
+        printf(", %zu passed and %zu failed",
+               num_instructions_passed,
+               num_instructions_tested - num_instructions_passed);
     printf("\n");
     if (flags->print_tested_instruction)
         printf("\n");
@@ -286,10 +298,18 @@ static int test_instructions(size_t num_instructions,
 
 static bool is_valid_op(u8 op)
 {
-    u8 invalid_ops[] = { 0xcb,                          // Special case (prefix)
-                         0xd3, 0xdb, 0xdd,
-                         0xe3, 0xe4, 0xeb, 0xec, 0xed,
-                         0xf4, 0xfc, 0xfd };
+    u8 invalid_ops[] = {0xcb, // Special case (prefix)
+                        0xd3,
+                        0xdb,
+                        0xdd,
+                        0xe3,
+                        0xe4,
+                        0xeb,
+                        0xec,
+                        0xed,
+                        0xf4,
+                        0xfc,
+                        0xfd};
 
     for (size_t i = 0; i < sizeof(invalid_ops); i++)
         if (invalid_ops[i] == op)
@@ -304,25 +324,26 @@ static bool is_valid_op_cb(u8 op)
     return true;
 }
 
-static void print_coverage(bool *op_table, bool (*is_valid)(u8),
-        const char *prefix)
+static void print_coverage(bool *op_table, bool (*is_valid)(u8), const char *prefix)
 {
     unsigned num_tested_ops, total_valid_ops;
 
     num_tested_ops = 0, total_valid_ops = 0;
-    for (int op = 0; op <= 0xff; op++) {
+    for (int op = 0; op <= 0xff; op++)
+    {
         if (op_table[op])
             num_tested_ops++;
         if (is_valid(op))
             total_valid_ops++;
     }
 
-    printf("Successfully tested %d/%d %sopcodes\n", num_tested_ops,
-            total_valid_ops, prefix);
+    printf("Successfully tested %d/%d %sopcodes\n", num_tested_ops, total_valid_ops, prefix);
 
-    if (num_tested_ops < total_valid_ops) {
+    if (num_tested_ops < total_valid_ops)
+    {
         printf("\nTable of untested/incorrect %sopcodes:\n", prefix);
-        for (int op = 0; op <= 0xff; op++) {
+        for (int op = 0; op <= 0xff; op++)
+        {
             if (op_table[op] || !is_valid(op))
                 printf("-- ");
             else
@@ -353,9 +374,8 @@ static int test_all_instructions(void)
     return 0;
 }
 
-
-int tester_run(struct tester_flags *app_flags,
-               struct tester_operations *app_tcpu_ops) {
+int tester_run(struct tester_flags *app_flags, struct tester_operations *app_tcpu_ops)
+{
     flags = app_flags;
     tcpu_ops = app_tcpu_ops;
 
@@ -366,20 +386,25 @@ int tester_run(struct tester_flags *app_flags,
 }
 
 // Function takes string and only enables matching instruction
-void set_test(char *mnem) {
+void set_test(char *mnem)
+{
     size_t num_instructions = sizeof(instructions) / sizeof(instructions[0]);
     bool found = false;
-    for (size_t i = 0; i < num_instructions; i++) {
-        if (strcmp(instructions[i].mnem, mnem) == 0) {
+    for (size_t i = 0; i < num_instructions; i++)
+    {
+        if (strcmp(instructions[i].mnem, mnem) == 0)
+        {
             instructions[i].enabled = true;
             found = true;
-        } else {
+        }
+        else
+        {
             instructions[i].enabled = false;
         }
     }
-    if (!found) {
+    if (!found)
+    {
         printf("Instruction %s not found\n", mnem);
         exit(1);
     }
 }
-
